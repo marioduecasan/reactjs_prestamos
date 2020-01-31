139e15959061c9557ba046e46adc35ca22315356
marioduecasan <duecasan2204@gmail.com>
2020-01-31 10:19:49 -0500

    subiendo proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState} from 'react';
+import Header from './componentes/header';
+import Formulario from './componentes/formulario';
+import Mensaje from './componentes/mensaje';
+import Resultado from './componentes/resultado';
+import Spinner from './componentes/spinner'
 
 function App() {
+
+  //definer el state
+  const [cantidad, guardarCantidad] = useState(0);
+  const [plazo, guardarPlazo] = useState('');
+  const [total, guardarTotal] = useState(0);
+  const [cargando, guardarCargando] = useState(false);
+  
+  let componente;
+
+  if(cargando){
+    componente = <Spinner/>
+  }else if(total === 0){
+    componente = <Mensaje/>
+  }else{
+    componente = <Resultado
+                  total = {total}
+                  plazo = {plazo}
+                  cantidad = {cantidad}
+                />
+    }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header
+        titulo = "cotizador de prestamos"
+      />
+      <div className='container'>
+        <Formulario
+          cantidad = {cantidad}
+          guardarCantidad = {guardarCantidad}
+          plazo = {plazo}
+          guardarPlazo = {guardarPlazo}
+          guardarTotal = {guardarTotal}
+          guardarCargando = {guardarCargando}
+        />
+        <div className="mensajes">
+          {componente}
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
